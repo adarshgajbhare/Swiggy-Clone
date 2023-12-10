ee98275ce6797769839bec42dc63722345d9f385
adarshgajbhare <adarshgajbhare@gmail.com>
2023-12-10 10:45:44 +0530

    added pune lat and lon

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const CARD_IMG =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const PUNE_API =
-"https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.516726&lng=73.856255&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
   export const MUMBAI_API = 
 "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"; 
