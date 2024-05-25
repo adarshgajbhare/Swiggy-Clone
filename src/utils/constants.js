577b3b9f7b1d0f46f31a22a36a932b9fb28cc110
adarshgajbhare <adarshgajbhare@gmail.com>
2024-05-25 12:48:28 +0530

    Added json data and logic to get data from local files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,6 @@ export const RES_MENU_API=
  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=";
 
 
-const CORS_URL = "https://corsproxy.org/?"
-
 export const getCityAPI = (lat, lng) => {
 
 
