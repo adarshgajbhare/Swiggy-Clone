20fc1e100dfb999f3f87a03ad27ec9355765b4f8
adarshgajbhare <adarshgajbhare@gmail.com>
2024-05-25 13:01:47 +0530

    Added local json for resturant menu

--- src/utils/useRestaurantMenus.js
+++ src/utils/useRestaurantMenus.js
@@ -9,11 +9,34 @@ const useRestaurantMenus = (resId) => {
   }, []);
 
   const fetchResMenu = async () => {
+    try {
     const data = await fetch(`${RES_MENU_API}/${resId}`);
     const json = await data.json();
+
+    if(json) {
     setResMenu(json.data);
-  };
+  }else {
+    throw new Error("API returned no data");
+  }
+} catch (error){
+  console.error("Not able to fetch data from siwggy :", error);
+  console.log("fetching data from local file...")
+
+  const localResponse = await fetch('https://raw.githubusercontent.com/adarshgajbhare/Swiggy-Clone/main/src/utils/pl.json');
+
+
+  const localJson = await localResponse.json();
+  if(localJson)
+     {
+    setResMenu(localJson.data);
+  }
+  else {
+    throw new Error("Local file returned no data");
+  }
   
+}
+
+  }
   return resMenu;
 };
 
